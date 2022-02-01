228dc119cc571ee8e1f5c54d73f38ed9faa0f668
bimalkanadeeshan <mwbnadee@GMAIL.COM>
2022-02-01 15:26:47 +0530

    Activities basic ui/ux created

--- app/src/main/java/com/example/bimalka/authenticatorapp/LoginActivity.java
+++ app/src/main/java/com/example/bimalka/authenticatorapp/LoginActivity.java
@@ -2,14 +2,14 @@ package com.example.bimalka.authenticatorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
-
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
     }
 }
--- app/src/main/java/com/example/bimalka/authenticatorapp/RegisterActivity.java
+++ app/src/main/java/com/example/bimalka/authenticatorapp/RegisterActivity.java
@@ -3,13 +3,12 @@ package com.example.bimalka.authenticatorapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         getSupportActionBar().hide();
     }
 }
